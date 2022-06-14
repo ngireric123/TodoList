b886c9ca52b04120318a326d62d16db5032fa7ed
ngireric123 <ngireric123@gmail.com>
2022-06-14 11:46:58 +0200

    add controlled component inputs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Header from './Header';
+import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 
@@ -38,6 +39,7 @@ function App() {
     <div className="App">
       {/* props decralation */}
       <Header title="Grocery List" />
+      <AddItem />
       <Content
         items={items}
         handleCheck={handleCheck}
--- src/ItemList.js
+++ src/ItemList.js
@@ -1,23 +1,16 @@
-import React from 'react'
+import React from 'react';
+import LineItem from './LineItem';
 
 const ItemList = ({ handleDelete, handleCheck, items }) => {
     return (
         <ul>
             {items.map((item) => (
-                <li className="item" key={item.id}>
-                    <input
-                        type="checkbox"
-                        onChange={() => handleCheck(item.id)}
-                        checked={item.checked}
-                    />
-                    <label
-                        style={(item.checked) ? { textDecoration: 'line-through' } : null}
-                        onDoubleClick={() => handleCheck(item.id)}
-                    >{item.item}</label>
-                    <button
-                        onClick={() => handleDelete(item.id)}
-                    >Delete</button>
-                </li>
+                <LineItem
+                    key={item.id}
+                    item={item}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
             ))}
         </ul>
     )
